8a55d1178f76bdde1a8dbffd61a55966abc9927f
Dyneteve <sound0020@gmail.com>
2021-08-26 20:50:19 +0530

    surya: Enable AOSP surfaceflinger
    * AOSP SurfaceFlinger is smoother than CAF
    
    Signed-off-by: rayrzy <vincentrayananda@gmail.com>
    Change-Id: I82dfcbb3ed35c3a05f1d6b7d0b424724f8f08d73
    Signed-off-by: Fakeriz <fakeriz.id@gmail.com>

--- product_prop.mk
+++ product_prop.mk
@@ -20,12 +20,31 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.charger.enable_suspend=true
 
-# Graphics
+# Graphics/Display (SF)
 PRODUCT_PRODUCT_PROPERTIES += \
+    debug.sf.early_phase_offset_ns=1500000 \
+    debug.sf.early_app_phase_offset_ns=1500000 \
+    debug.sf.early_gl_phase_offset_ns=3000000 \
+    debug.sf.early_gl_app_phase_offset_ns=15000000 \
+    ro.surface_flinger.use_color_management=true \
+    ro.surface_flinger.set_idle_timer_ms=80 \
+    ro.surface_flinger.set_touch_timer_ms=200 \
+    ro.surface_flinger.set_display_power_timer_ms=1000 \
+    ro.surface_flinger.support_kernel_idle_timer=true \
+    ro.surface_flinger.supports_background_blur=1 \
+    ro.surface_flinger.use_frame_rate_api=true \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
+    ro.surface_flinger.use_smart_90_for_video=true \
+    ro.surface_flinger.protected_contents=true \
     debug.sf.disable_backpressure=1 \
     debug.sf.latch_unsignaled=1 \
     ro.surface_flinger.has_wide_color_display=false
+
+# Display (Native blur)
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.blurs_are_expensive=1 \
+    persist.sys.sf.disable_blurs=1
 
 # Smart fps switching
 PRODUCT_PRODUCT_PROPERTIES += \
